f76b68ea0835a0e3bc97cf80bc6dcc178f9e1144
Michael Peter <michaelhpet@gmail.com>
2025-03-31 10:06:02 +0100

    feat: add req validation for products CRUD

--- src/app/v1/products/validation.ts
+++ src/app/v1/products/validation.ts
@@ -6,3 +6,40 @@ export const validateGETProducts = celebrate({
     limit: Joi.number().min(1),
   },
 });
+
+export const validateGETProduct = celebrate({
+  params: {
+    id: Joi.string().required(),
+  },
+});
+
+export const validatePOSTProduct = celebrate({
+  body: {
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    category: Joi.string().required(),
+    price: Joi.number().required(),
+    sku: Joi.string().required(),
+    stock_status: Joi.string().valid("in_stock", "out_of_stock").required(),
+  },
+});
+
+export const validatePUTProduct = celebrate({
+  params: {
+    id: Joi.string().required(),
+  },
+  body: {
+    name: Joi.string(),
+    description: Joi.string(),
+    category: Joi.string(),
+    price: Joi.number(),
+    sku: Joi.string(),
+    stock_status: Joi.string().valid("in_stock", "out_of_stock"),
+  },
+});
+
+export const validateDELETEProduct = celebrate({
+  params: {
+    id: Joi.string().required(),
+  },
+});
